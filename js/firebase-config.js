299ee0799c8a6360785e74e20ec2ec29d8b5c2b1
mc-coi <zmccoy@gmail.com>
2026-03-02 12:29:51 -0500

    removed Firebase imports throwing errors

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,6 @@
 // ============================================
 // Replace these values with your own Firebase project settings
 // Get them from: https://console.firebase.google.com
-// Project Settings > Your Apps > Firebase SDK snippet > Config
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
